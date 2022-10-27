59cf24ce38438a07dbc565893a04bd8281d1b26b
marc7s <marcus.schagerberg@gmail.com>
2022-10-27 03:10:01 +0200

    Updated cors link

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 const cors = require('cors');
 
 app.use(cors({
-    origin: 'http://localhost:4200'
+    origin: 'http://old.schagerberg.com/Leaderboard'
 }));
 
 app.use(express.json());
